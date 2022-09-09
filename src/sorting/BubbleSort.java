8a154a76571aad7d577f8b51be11ecc61e9b5f07
Karurot <kartikey230@gmail.com>
2022-09-09 09:38:16 +0530

    Bubble sort

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,10 +1,29 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    static void bubble(int[] arr){
+        boolean swap;
+        for(int i=0;i<arr.length;i++){
+            swap = false;
+            for (int j=1;j<arr.length-i;j++){
+                if (arr[j] <arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swap = true;
+                }
+            }
+            if (!swap)
+                break;
+        }
+    }
 
-	}
-
+    public static void main(String[] args) {
+        int arr[] = {4,5,7,8,6};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
